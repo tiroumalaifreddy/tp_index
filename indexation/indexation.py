c562ec6eb1d0a645764fb5800db24b000787b56c
tiroumalaifreddy <ftiroumala@gmail.com>
2023-01-23 16:05:38 +0100

    all functions good

--- indexation/indexation.py
+++ indexation/indexation.py
@@ -6,6 +6,14 @@ import numpy
 from urllib.parse import urlsplit
 import requests
 from nltk.tokenize import word_tokenize
+from nltk import FreqDist
+from collections import defaultdict
+import json
+from statistics import mean
+
+def import_json_to_list(json_file_path):
+    f = open(json_file_path)
+    return json.load(f)
 
 def extract_title(url_page : str):
     try:
@@ -22,6 +30,26 @@ def tokenize(string):
 def tokenize_list(list_string : list):
     list_token = []
     for string in list_string:
-        list_token.append(word_tokenize(string))
+        list_token.append(word_tokenize(string.lower()))
     return list_token
 
+def word_frequency(tokens : list):
+    frequence = FreqDist(tokens)
+    return frequence
+
+def create_inverted_index(list_token):
+    index = defaultdict(list)
+    for i, tokens in enumerate(list_token):
+        frequence = dict(word_frequency(tokens))
+        for token in tokens:
+            count = frequence[token]
+            index[token].append(f"{i}:{count}")
+    for k,v in index.items():
+        index[k] = [*set(v)]
+    return index
+
+def create_stats(list_token):
+    number_of_docs = len(list_token)
+    number_of_tokens = sum(len(l) for l in list_token)
+    mean_per_doc = mean([len(l) for l in list_token])
+    return dict([(i, locals()[i]) for i in ('number_of_docs', 'number_of_tokens', 'mean_per_doc')])
